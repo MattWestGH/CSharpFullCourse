602afa52b936d0ebe05357ef72112bbdf5d0a111
MattWestGH <workmailmattwest@gmail.com>
2025-01-31 17:56:09 +0200

    Practicing Version control

--- CSharpFullCourse/Program.cs
+++ CSharpFullCourse/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,8 +23,8 @@ namespace CSharpFullCourse
                 Console.ReadLine();
             */
 
-            for (int i = 1; i <= 15; i++) { 
-            
+            for (int i = 1; i <= 15; i++) {
+                //this is to test version control
             };
         }
     }
